8bc92ca6012a300bfb319af07a200051e2f86119
Prachi_Anand <prachianand1706@gmail.com>
2026-07-24 00:50:15 +0530

    Time: 0 ms (100%), Space: 42.2 MB (14.92%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -1,15 +1,12 @@
 class Solution {
     public int getSum(int a, int b) {
-        int sum = (a^b);
-        int carry  = (a&b);
-        while(carry!=0){
-            sum=(a^b);
-            carry = (a&b)<<1;
+        while(b!=0){
+            int sum = (a^b);
+            int carry = (a&b)<<1;
             a=sum;
             b=carry;
-
         }
-        return sum;
+        return a;
         
     }
 }
